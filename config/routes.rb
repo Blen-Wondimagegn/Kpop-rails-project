72d7a6854c6d52f9a54567a9d0da7562ab96d6a6
blen_meku <mekuriablen@gmail.com>
2020-11-06 00:46:51 -0800

    added REst and custome routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {registrations: 'registrations' }
+ 
+  devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks' } do
+      resources :glams  
+  end
+  
+  resources :artists do
+    resources :glams  
+  end
 
-  resources :artists
+    resources :glams  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get '/female', to: "artists#female"
+   get '/male', to: "artists#male"
   root to: "artists#index"
 end
