ca2156eeeb7a510c57e01c608f9809bf63901145
blen_meku <mekuriablen@gmail.com>
2020-11-08 18:53:27 -0800

    modified

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,16 @@ Rails.application.routes.draw do
       resources :glams  
   end
   
+   resources :glams  
+
   resources :artists do
     resources :glams  
   end
 
     resources :glams  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/female', to: "artists#female"
+   get '/female', to: "artists#female"
    get '/male', to: "artists#male"
+  #  get '/home', to: "artists#home"
   root to: "artists#index"
 end
